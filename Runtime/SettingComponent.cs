3670eeff9b030a4ece80d8935e0a5f14fbc35842
CuteRogue <andhika.rogue@gmail.com>
2025-05-28 22:15:50 +0700

    add serialized odin support for setting component

--- Runtime/SettingComponent.cs
+++ Runtime/SettingComponent.cs
@@ -1,8 +1,16 @@
+#if TF_HAS_TFODINEXTENDER
+using Sirenix.OdinInspector;
+#else
 using UnityEngine;
+#endif
 
 namespace TF.SettingMenu
 {
+    #if TF_HAS_TFODINEXTENDER
+    public abstract class SettingComponent : SerializedScriptableObject
+    #else
     public abstract class SettingComponent : ScriptableObject
+    #endif
     {
         // empty, just used for classification of inheritance
 
